c3cf2b42fcd4481b9234832dc67c374a9e7161f4
xqq <103427292+iuQQ@users.noreply.github.com>
2022-05-11 06:14:34 +0800

    Add files via upload

--- form.php
+++ form.php
@@ -1,11 +1,14 @@
 <?php
- $username=$_GET['username'];
- $password=$_GET['password'];
- $email=$_GET['email'];
+ $username=$_POST['username'];
+ $password=$_POST['password'];
+ $email=$_POST['email'];
+ $phone=$_POST[phone]
  
  echo $username;
  echo "<br>";
  echo $password;
  echo "<br>";
  echo $email;
+ echo "<br>";
+ echo $phone;
 ?>
