0745752dfc206cc3e8713ece547f648ab3a20321
xqq <103427292+iuQQ@users.noreply.github.com>
2023-02-18 07:45:20 +0800

    Add files via upload

--- v.js
+++ v.js
@@ -1,7 +1,95 @@
 get_new_version({
 	name:'文件管理器',
-	Version:'1.093',
+	Version:'1.094',
 	page:'https://iuqq.github.io/',
-	Introduction:'功能更新，优化排版，优化视觉效果，优化UI,问题修复 ←_←',
+	Introduction:'增加加载页面显示具体加载内容，对菜单栏功能进行完善 ←_←',
 	download:'https://iuqq.github.io/fi_ma.apk'
 })
+/*
+    1. 微博API：https://open.weibo.com/wiki/API
+    2. 豆瓣开发者API：https://developers.douban.com/wiki/?title=API
+    3. 谷歌地图API：https://developers.google.com/maps/
+    4. Twitter API：https://developer.twitter.com/en/docs
+    5. 聚合数据：https://www.juhe.cn/docs/api#/?menu
+    6. 新浪微博开放平台：http://open.weibo.com/
+    7. Bing Maps API：https://www.bingmapsportal.com/
+    8. 百度地图API：http://lbsyun.baidu.com/index.php
+    9. 知乎API：https://www.zhihu.com/api
+    10. 高德地图API：http://lbs.amap.com/
+    11. OpenWeatherMap API：https://openweathermap.org/api
+    12. AccuWeather API：http://www.accuweather.com/zh/zh/home/help/api
+    13. Flickr API：https://www.flickr.com/services/api/
+    14. Soundcloud API：https://developers.soundcloud.com/
+    15. Youtube API：https://developers.google.com/youtube/
+    16. Weather Underground API：https://www.wunderground.com/weather/api/
+    17. Instagram API：https://www.instagram.com/developer/
+    18. Iron.io API：https://www.iron.io/api/
+    19. Dropbox API：https://www.dropbox.com/developers
+    20. Wolfram Alpha API：https://products.wolframalpha.com/api/
+    21. 百度新闻api：http://news.baidu.com/
+    22. Mailchimp API：https://developer.mailchimp.com/
+    23. Github API：https://developer.github.com/v3/
+    24. Evernote API：https://dev.evernote.com/
+    25. Stripe API：https://stripe.com/docs/api
+    26. Shopify API：https://docs.shopify.com/api
+    27. 搜狐新闻api：http://developer.play.sohu.com/index.shtml
+    28. Reddit API：https://www.reddit.com/dev/api/
+    29. Yelp Fusion API：https://www.yelp.com/fusion
+    30. Tumblr API：https://www.tumblr.com/docs/en/api
+    31. Foursquare API：https://developer.foursquare.com/
+    32. 必应图片api：http://pic.biying.com/docs/
+    33. Goole Place API：https://developers.google.com/places/
+    34. Microsoft Translator API：https://www.microsoft.com/en-us/translator/
+    35. 必应翻译api：http://www.bingdic.cn/
+    36. SendGrid API：https://sendgrid.com/docs/API_Reference/index.html
+    37. OpenStreetMap：http://wiki.openstreetmap.org/wiki/API_v0.6
+    38. Reddit2api：http://www.reddit2api.com/
+    39. OpenCorporates API：https://api.opencorporates.com/
+    40. Last.fm API：http://www.last.fm/api
+    41. Paypal API：https://developer.paypal.com/docs/api/
+    42. LinkedIn API：https://developer.linkedin.com/
+    43. twilio API：https://www.twilio.com/docs/api
+    44. Impinj speedway API：https://developer.impinj.com/
+    45. 网易云音乐API：https://github.com/Binaryify/NeteaseCloudMusicApi
+    46. 阿里云API：https://developer.aliyun.com/api
+    47. Banking API：https://www.bankingapi.io/
+    48. SoundCloud API：https://developers.soundcloud.com/docs/api/guide
+    49. PublicData API：https://publicdata.org/developer
+    50. Slack API：https://api.slack.com/
+    51. Intercom API：https://developers.intercom.com/
+    52. gRPC API：https://grpc.io/docs/tutorials/basic/go/
+    53. Watson API：https://www.ibm.com/watson/developer/
+    54. Google Custom Search API：https://developers.google.com/custom-search/
+    55. Salesforce API：https://developer.salesforce.com/
+    56. OpenTable API：https://www.opentable.com/developers
+    57. Windows VFX API：https://developers.microsoft.com/en-us/windows/vfx
+    58. Firebase API：https://firebase.google.com/docs/
+    59. Gravatar API：https://www.gravatar.com/site/implement/
+    60. MapBox API：https://www.mapbox.com/api-documentation/
+    61. QuickBooks API：https://developer.intuit.com/docs/00_quickbooks_online/2_build/10_develop/v2
+    62. Coinbase API：https://developers.coinbase.com/
+    63. Heroku API：https://devcenter.heroku.com/
+    64. Plaid API：https://plaid.com/docs/
+    65. Cloudflare API：https://developers.cloudflare.com/
+    66. Facebook Graph API：https://developers.facebook.com/docs/graph-api/
+    67. Fitbit API：https://dev.fitbit.com/
+    68. Twilio API：https://www.twilio.com/docs/api
+    69. IBM Watson API：https://www.ibm.com/watson/developer/
+    70. OpenEXCHANGE API：https://developer.openexchangerates.org/
+    71. Oracle API：https://www.oracle.com/webfolder/technetwork/tutorials/tut_jdbc/jdbc_api.htm
+    72. SendGrid API：https://sendgrid.com/docs/API_Reference/index.html
+    73. Stripe API：https://stripe.com/docs/api
+    74. Twilio API：https://www.twilio.com/docs/api
+    75. Ustream API：https://developer.ustream.tv/
+    76. JSONPlaceholder：https://jsonplaceholder.typicode.com/
+    77. Coinbase API：https://developers.coinbase.com/
+    78. Spotify API：https://developer.spotify.com/documentation/web-api/
+    79. Eventbrite API：https://www.eventbrite.com/platform/api
+    80. Box API：https://developer.box.com/
+    81. Dropbox API：https://www.dropbox.com/developers
+    82. JustEat API：https://api-portal.justeat.com/
+    83. MailChimp API：https://developer.mailchimp.com/
+    84. Stripe Connect：https://stripe.com/docs/connect
+    85. Box API：https://developer.box.com/docs/
+    86. Twilio Programmable Voice：https://www.twilio
+*/
