a910735168c42f41d835ebf02e4d6f9c767610ab
xqq <103427292+iuQQ@users.noreply.github.com>
2023-02-17 11:44:06 +0800

    Add files via upload

--- v.js
+++ v.js
@@ -1,7 +1,7 @@
 get_new_version({
 	name:'文件管理器',
-	Version:'1.092',
+	Version:'1.093',
 	page:'https://iuqq.github.io/',
-	Introduction:'1.09.2版本：在1.09.1版本的基础上优化了文件加载问题，优化了部分UI界面 ←_←',
+	Introduction:'功能更新，优化排版，优化视觉效果，优化UI,问题修复 ←_←',
 	download:'https://iuqq.github.io/fi_ma.apk'
 })
